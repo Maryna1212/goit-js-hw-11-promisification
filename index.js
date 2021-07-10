e45865b4de184a66bae5e91f6b83db89df0ac46d
Maryna Vereshchak <vereschak12@gmail.com>
2021-07-11 01:33:15 +0300

    add title to index.html

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
 // через ms миллисекунд.Значением исполнившегося промиса должно быть то кол - во миллисекунд 
 // которое передали во время вызова функции delay.
 
-// const delay = ms => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => resolve(ms), ms);
-//     });
-// };
+const delay = ms => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(ms), ms);
+    });
+};
 
-// const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
 
 // Task 2
 // Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback,
@@ -48,48 +48,48 @@
 // Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess 
 // и onError, а принимала всего один параметр transaction и возвращала промис.
 
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
+// const randomIntegerFromInterval = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// };
 
-const makeTransaction = transaction => {
-    return new Promise((resolve, reject) => {
-        const delay = randomIntegerFromInterval(200, 500);
+// const makeTransaction = transaction => {
+//     return new Promise((resolve, reject) => {
+//         const delay = randomIntegerFromInterval(200, 500);
 
-        setTimeout(() => {
-            const canProcess = Math.random() > 0.3;
+//         setTimeout(() => {
+//             const canProcess = Math.random() > 0.3;
 
-            if (canProcess) {
-                resolve({ id: transaction.id, time: delay });
-            } else {
-                reject(transaction.id);
-            }
-        }, delay);
-    });
-};
+//             if (canProcess) {
+//                 resolve({ id: transaction.id, time: delay });
+//             } else {
+//                 reject(transaction.id);
+//             }
+//         }, delay);
+//     });
+// };
 
-const logSuccess = ({ id, time }) => {
-    console.log(`Transaction ${id} processed in ${time}ms`);
-};
+// const logSuccess = ({ id, time }) => {
+//     console.log(`Transaction ${id} processed in ${time}ms`);
+// };
 
-const logError = id => {
-    console.warn(`Error processing transaction ${id}. Please try again later.`);
-};
-/*
- * Должно работать так
- */
-makeTransaction({ id: 70, amount: 150 })
-    .then(logSuccess)
-    .catch(logError);
+// const logError = id => {
+//     console.warn(`Error processing transaction ${id}. Please try again later.`);
+// };
+// /*
+//  * Должно работать так
+//  */
+// makeTransaction({ id: 70, amount: 150 })
+//     .then(logSuccess)
+//     .catch(logError);
 
-makeTransaction({ id: 71, amount: 230 })
-    .then(logSuccess)
-    .catch(logError);
+// makeTransaction({ id: 71, amount: 230 })
+//     .then(logSuccess)
+//     .catch(logError);
 
-makeTransaction({ id: 72, amount: 75 })
-    .then(logSuccess)
-    .catch(logError);
+// makeTransaction({ id: 72, amount: 75 })
+//     .then(logSuccess)
+//     .catch(logError);
 
-makeTransaction({ id: 73, amount: 100 })
-    .then(logSuccess)
-    .catch(logError);
+// makeTransaction({ id: 73, amount: 100 })
+//     .then(logSuccess)
+//     .catch(logError);
